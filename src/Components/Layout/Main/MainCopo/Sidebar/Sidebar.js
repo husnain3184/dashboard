e7da64c30b5a3a273d658b49ff3288bb9133b1bd
husnain.raza <husnain.raza@genetechsolutions.com>
2021-12-08 15:09:45 +0500

    changes

--- src/Components/Layout/Main/MainCopo/Sidebar/Sidebar.js
+++ src/Components/Layout/Main/MainCopo/Sidebar/Sidebar.js
@@ -1,9 +1,17 @@
+import React,{useState, useEffect} from "react"
 import SideNavHeader from './SidebarCompo/SideNavHeader'
 import NavbarCollapse from './SidebarCompo/NavbarCollapse'
 import SideNavFooter from './SidebarCompo/SideNavFooter'
 
 
 const Sidebar = () =>{
+
+        if(window.location.pathname === "/signin"){
+           
+            return null;
+        }
+
+   
     return(
         <>
             <aside className="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
@@ -12,6 +20,8 @@ const Sidebar = () =>{
               <SideNavFooter/>    
           </aside>
         </>
-    )}
+    )
+
+}
     
 export default Sidebar
